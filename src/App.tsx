b20c4b34999c64f483d67b2297b8e9c18eff8d6e
SonNguyenPk <sonnguyen.pk1301@gmail.com>
2022-06-16 07:02:36 +0700

    add mainlayout
    - add main layout
    - add page router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import '~antd/dist/antd.css';
-import './App.css';
+import 'antd/dist/antd.css';
+import { Route, Routes } from 'react-router-dom';
+//import './App.css';
+import MainLayout from './components/main-layout/main-layout.component';
+import CarList from './pages/car-list/car-list';
+import Home from './pages/home/home';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Routes>
+				<Route path='' element={<MainLayout />}>
+					<Route path='/' element={<Home />}></Route>
+					<Route path='/car-list' element={<CarList />}></Route>
+				</Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
